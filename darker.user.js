ac4d6d97d45793d1a6adc3ff1518215fa6b705f6
Szero <singleton@tfwno.gf>
2017-02-07 23:23:07 +0100

    added fonts and css

--- darker.user.js
+++ darker.user.js
@@ -4,15 +4,16 @@
 // @version      0.1
 // @description  change of volafile experience
 // @author       Your mom
+// @grant        GM_addStyle
 // @match        https://*.volafile.io/r/*
-// @run-at       document-start
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
-    console.log("i shiggy diggy");
-        var UI = document.getElementById("show_search_ui");
-        var topBar = document.getElementById("header_row1");
-        var bottomBar = document.getElementById("header_row2");
-        var holdUI = bottomBar.removeChild(UI);
-        topBar.appendChild(holdUI);
+
+    var UI = document.getElementById("show_search_ui");
+    var topBar = document.getElementById("header_row1");
+    var bottomBar = document.getElementById("header_row2");
+    var holdUI = bottomBar.removeChild(UI);
+    topBar.appendChild(holdUI);
 })();
