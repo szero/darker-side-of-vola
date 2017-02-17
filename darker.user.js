91ec0ce7524f604754965860ad23c77beb7d5f47
Szero <singleton@tfwno.gf>
2017-02-17 02:41:15 +0100

    updated js and small css fix
    script behaves diferently on pcs with screen width smaller than 1650px

--- darker.user.js
+++ darker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Darker Side of Volafile
 // @namespace    i have none
-// @version      0.3
+// @version      0.4
 // @description  change of volafile experience
 // @author       Your mom
 // @match        https://volafile.io/*
@@ -12,35 +12,45 @@
 (function () {
     "use strict";
     try {
-        let UI = document.getElementById("show_search_ui");
+    var UI,header,frame,bottomBar;
+    function bigRes() {
+        UI = document.getElementById("show_search_ui");
         let roomSh = document.getElementById("room_search");
-        let filters = document.getElementById("room_filters");
         let topBar = document.getElementById("header_row1");
-        let bottomBar = document.getElementById("header_row2");
+        bottomBar = document.getElementById("header_row2");
         let uploadBt = document.getElementById("upload_container");
         topBar.appendChild(bottomBar.removeChild(UI));
-        topBar.appendChild(bottomBar.removeChild(filters));
+        if (screen.width > 1650) {
+            let filters = document.getElementById("room_filters");
+            topBar.appendChild(bottomBar.removeChild(filters));
+        }
         topBar.appendChild(bottomBar.removeChild(roomSh));
         topBar.appendChild(bottomBar.removeChild(uploadBt));
-        let header = document.getElementById("header");
-        let frame = document.getElementById("files_frame");
+        header = document.getElementById("header");
+        frame = document.getElementById("files_frame");
         bottomBar.style.display = "none";
         header.style.height = "1.5em";
         frame.style.top = "1.8em";
+    }
+    if (screen.width < 1650) {
+        bigRes();
+        function close() {
+           frame.style.top = "1.8em";
+            header.style.height = "1.5em";
+            bottomBar.style.display = "none";
+        }
+        function filter() {
+            bottomBar.style.display = "block";
+            frame.style.top = "3.5em";
+            header.style.height = "3.5em";
+        }
+        let clear = document.getElementById("clearsearch");
+        clear.addEventListener("click", close);
+        UI.addEventListener("click", filter);
+    } else {
+        bigRes();
+    }
     } catch (err) { console.log("this is just for pages other than vola rooms") }
-    /*function close() {
-      frame.style.top = "1.8em";
-      header.style.height = "1.5em";
-      bottomBar.style.display = "none";
-      }
-      function filter() {
-      bottomBar.style.display = "block";
-      frame.style.top = "3.5em";
-      header.style.height = "3.5em";
-      }
-      let clear = document.getElementById("clearsearch");
-      clear.addEventListener("click", close);
-      UI.addEventListener("click", filter); */
     //GM will do this: http://unixpapa.com/js/dyna.html
     let newLink = document.createElement("link");
     newLink.rel = "stylesheet";
