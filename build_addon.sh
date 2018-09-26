8f667e3456b4114acc439f0928a486fb6172070f
Szero <singleton@tfwno.gf>
2018-09-26 03:26:33 +0200

    improve bump script readability

--- build_addon.sh
+++ build_addon.sh
@@ -6,15 +6,22 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [[ "$#" -ne 2 ]]; then
+    echo "Please specify exactly two arguments: First being the UserScript field"
+    echo "in the script file and second, the value you want it to be replaced with."
+    exit 1
+fi
+
 look_after="$1"
+replace_with="$2"
 raw_addon="./darker.js"
 addon="./extension/darker.user.js"
 meta="./extension/darker.meta.js"
 
 bump_version() {
-    sed -i -r "$(sed -n "/$look_after/=" "$2")s/^([\/]+\s*@$look_after\s+).*$/\1$1/" "$2"
+    sed -i -r "$(sed -n "/$look_after/=" "$2")s/^([\\/]+\\s*@$look_after\\s+).*$/\\1$1/" "$2"
 }
 
-bump_version "$2" "$raw_addon"
-bump_version "$2" "$meta"
+bump_version "$replace_with" "$raw_addon"
+bump_version "$replace_with" "$meta"
 stuff2str "$raw_addon" "$addon"
